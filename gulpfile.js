22d38423ed2a5be046f97bb2166b3a63dd1138a8
ratherbm8 <Brian@Brians-iMac-2.local>
2015-10-15 08:48:16 -0700

    Added coffee script files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-	gutil.log('workflows are awesome');
+
+gulp.task('coffee', function() {
+	// body...
+	gulp.src('components/coffee/tagline.coffee')
+	.pipe(coffee({bare: true})
+		.on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
+		)
 });
